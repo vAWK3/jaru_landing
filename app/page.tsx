5debe13e1faa190d5909d7c3e07a6b6249117213
Basel Sader <baselsader@protonmail.com>
2024-11-10 01:51:49 +0200

    fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Banner from './components/Banner/Banner';
-import Companies from './components/Companies/Companies';
 import Tabs from './components/Services/Services';
-import Mentor from './components/Mentor/Mentor';
-import Students from './components/Students/Students';
-import Newsletter from './components/Newsletter/Newsletter';
 import About from './about/page';
 
 export default function Home() {
